c0fda2b6ebfd77f4e015f28d3f3e598dd4bf04c1
AnnaMintel <annikamintel99@yandex.ru>
2022-10-01 01:05:31 +0300

    feat: added new resolved tasks

--- src/lessons/lesson2/lesson2.tsx
+++ src/lessons/lesson2/lesson2.tsx
@@ -1,4 +1,4 @@
-console.log('lesson 2');
+import React from "react";
 
 // Lexical environment
 // http://jsflow.org/docs/lex-env/
@@ -21,10 +21,19 @@ console.log('lesson 2');
 // https://www.youtube.com/watch?v=Kuq6oIN3PH0
 
 
-// Task 01
-// Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
+const Lesson2: React.FunctionComponent = () => {
 
-// Task 02
+
+                      // Task 01
+// Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
+function sum(n: number):any{
+    return function(n2:number){
+        return n + n2;
+    }
+}
+console.log(sum(3)(6));
+
+                      // Task 02
 // Реализовать функцию makeCounter которая работает следующим образом:
 // const counter = makeCounter();
 // counter(); // 1
@@ -32,16 +41,21 @@ console.log('lesson 2');
 // const counter2 = makeCounter();
 // counter2(); // 1
 // counter(); // 3
+function makeCounter(){
+    let count = 0;
+    return function(){
+        return ++count;
+    }
+}
+const counter = makeCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+const counter2 = makeCounter()
+console.log(counter2());
 
-// Task 03
-// Переписать функцию из Task 02 так, что бы она принимала число в качестве аргумента и это число было стартовым значением счетчика
-// и возвращала следующий объект методов:
-// increase: +1
-// decrease: -1
-// reset: установить счетчик в 0;
-// set: установить счетчик в заданное значение;
-
-// Task 04*
+                      // Task 03*
 // Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
 // и что бы корректно работали следующие вызовы:
 // 1) superSum(0) //0
@@ -51,13 +65,42 @@ console.log('lesson 2');
 // 5) superSum(3)(2,5)(3) //10
 // 6) superSum(3)(2,5)(3,9) //10
 
-// P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
 
-// Task 05
+                       // Task 04
 // решить все задачи по рекурсии которые даны в конце статьи https://learn.javascript.ru/recursion
 
-// Task 06
-// написать функцию, которая повторяет функционал метода flat массива на всю глубину.
+// 1 Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
+function sumTo(n:number):any {
+    if (n == 1) {
+        return 1
+    }
+    return n + sumTo(n - 1)
+}
+console.log(sumTo(4));
 
-// just a plug
-export default () => {};
+// 2 - Определение факториала
+function factorial (n:number):any {
+    if (n ==1) {
+        return 1
+    }
+    return n * factorial(n-1)
+}
+console.log(factorial(5));
+
+// 3 - определение числа Фибоначчи
+function fibonachchi (n:number):any {
+    return n <= 1 ? n : fibonachchi(n-1) + fibonachchi(n-2);
+}
+console.log(fibonachchi(7));
+
+
+                    // Task 05
+// написать функцию, которая повторяет функционал метода flat массива на всю глубину.
+ 
+
+return (
+    <div>sss</div>
+    );
+    };
+    
+export default Lesson2;
