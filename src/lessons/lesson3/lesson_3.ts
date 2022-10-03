82fbeb4704e920c3b2c3c3175721c262eb2d078c
AnnaMintel <annikamintel99@yandex.ru>
2022-10-03 21:10:23 +0300

    feat: worked with promises

--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -15,6 +15,82 @@ console.log('lesson 3');
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
 
+let prom = new Promise((resolve, reject) => {
+    // some code
+    let a = 10;
+    let b = 15;
+    let c = a + b;
+    setTimeout(resolve, 1000, 5);
+    // reject(0)
+});
+
+// then error then error
+prom.then(res => {
+    console.log(res);
+    return 58;
+}, err => {
+    console.log(err);
+    return 50;
+}).then(res2 => {
+    console.log('res2 ', res2);
+}, err2 => {
+    console.log('err2 ', err2);
+    return 40;
+})
+
+// then then then catch
+prom.then(res3 => {
+    console.log('res3 ', res3);
+    //@ts-ignore
+    return res3 + 58;
+})
+.then(res4 => {
+    console.log('res4 ', res4);
+    //@ts-ignore
+    return res4 + 68;
+})
+.catch(error => {
+    console.log("error ", error);
+})
+
+// then catch then catch
+prom.then(res5 => {
+    console.log('res5 ', res5);
+    //@ts-ignore
+    return res5 + 10;
+})
+.catch(error => {
+    console.log("error ", error);
+})
+.then(res6 => {
+    console.log('res6 ', res6);
+    //@ts-ignore
+    return res6 + 15;
+})
+.catch(error => {
+    console.log("error ", error);
+})
+
+
+// finally не может вернуть новый промис, не может изменить промис
+// используют для выполнения side effects (например, preloader)
+.then(res7 => {
+    console.log('res7 ', res7);
+    //@ts-ignore
+    return res7 + 15;
+})
+//@ts-ignore
+.finally(fin => {
+    console.log('finally ', fin);
+    return 100;
+})
+.then (res8 => {
+    console.log('res8 ', res8);
+    //@ts-ignore
+    return res8 + 15;
+})
+
+
 
 // just a plug
-export default ()=>{};
+export default () => { };
